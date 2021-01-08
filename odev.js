bff38435aee476b708cad057aeffe33cf85e54a6
Yavuz Ali Korkut Ustbas <yavuzalikorkut@outlook.com>
2021-01-08 22:43:35 +0000

    Add modal

--- odev.js
+++ odev.js
@@ -13,5 +13,27 @@ window.removePet = (id) => {
 };
 
 window.openPetDetail = (id) => {
-    console.log(id); // show details of the pet with given id
+    fetch(`${window.mockApiUrl}/${id}`).then((response) => response.json()).then((pet) => {
+      const petModal = `<div class="modal fade" id="exampleModal${pet.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                              <div class="modal-dialog">
+                                  <div class="modal-content">
+                                      <div class="modal-header">
+                                          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                      </div>
+                                      <div class="modal-body">
+                                          ${pet.name}
+                                      </div>
+                                      <div class="modal-footer">
+                                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>`
+      
+      let modalDiv = document.createElement("div");
+      modalDiv.innerHTML = petModal;
+      document.body.appendChild(modalDiv);
+      new bootstrap.Modal(modalDiv.querySelector(".modal")).show();
+    });
 };
